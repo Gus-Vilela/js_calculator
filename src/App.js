90cd6f137e4152ac6d6abaf955813c5b907780e4
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-04-27 19:30:27 -0300

    add calculator Style

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-	const[data] = useState([{id: 1, name: "zero"},{id: 2, name: "one"},{id: 3, name: "two"},{id: 4, name: "three"},{id: 5, name: "four"},{id: 6, name: "five"},{id: 7, name: "six"},{id: 8, name: "seven"},{id: 9, name: "eight"},{id: 10, name: "nine"}]);
-	const [state, setState] = useState(0);
+	
+	const[data] = useState([{id: 8, name: "seven", value: 7},{id: 9, name: "eight", value: 8},{id: 10, name: "nine", value: 9},
+			{id: 5, name: "four", value: 4},{id: 6, name: "five", value: 5},{id: 7, name: "six", value: 6},
+			{id: 2, name: "one", value: 1},{id: 3, name: "two", value: 2},
+			{id: 4, name: "three", value: 3},{id: 1, name: "zero", value:0}])
+	const[operators] = useState([{id: 11, name: "add", value: '+'},{id: 12, name: "subtract", value: '-'},
+			{id: 13, name: "multiply", value: '*'},{id: 14, name: "divide", value: '/'}])
+	const [state, setState] = useState({result:0, display:''});
 	return (
 		<div className='Calc'>
 			<div className='Calc-Display'>
-				{state}
+				{state.display}
 			</div>
 			<div className='Calc-Keypad'>
-				<button onClick={() => setState(0)}>AC</button>
-				<div className='Calc-Keypad-Operators'>
-				</div>
+				<button id="clear" className='ac' onClick={() => setState(({result:0, display:''}))}>AC</button>
 				<div className='Calc-Keypad-Numbers'>
-					{data.map((item) => { return <button key={item.id} onClick={() => setState((item.id-1))}>{item.name}</button> })}
+					{data.map((item) => { return <button key={item.id} id={item.name}>{item.value}</button> })}
+					<button id='decimal'>.</button>
+					<button id='equals'>=</button>
 				</div>
+				
+				<div className='Calc-Keypad-Operators'>
+					{operators.map((item) => { return <button className='operator' key={item.id} id={item.name}>{item.value}</button> })}
+				</div>
+				
 			</div>
 		</div>
 );}
