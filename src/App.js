66fb531d97d60bd11ecff7b9cc3fd7799c115e08
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-04-28 15:51:29 -0300

    add decimal operations, pass codecamp tests

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
 		formula: '',
 		});
 
+	const[activeOperators, setActiveOperators] = useState([]);
+
 	const[display, setDisplay] = useState('0');
 
 	const numberPressed = (e) => {
@@ -24,28 +26,49 @@ function App() {
 				setFormula({previusPressed: e.target.value, formula: ''});
 				setDisplay(e.target.value);
 			}else{
-				setDisplay(p => {if(p === '0' && e.target.value==='0'){
-					return p;
-				}else if(p === '0' && e.target.value !== '0'){
-					return e.target.value;
+				if(display.length > 12){
+					return;
 				}else{
-					return p + e.target.value;
-				}
-			})};
+					setActiveOperators([]);
+					setFormula(p => ({...p, previusPressed: e.target.value}));
+					setDisplay(p => {if(p === '0' && e.target.value==='0'){
+						return p;
+					}else if(p === '0' && e.target.value !== '0'){
+						return e.target.value;
+					}else{
+						return p + e.target.value;
+					}
+				})};
+			}
 	}
 
 	const operatorPressed = (e) => {
 		if(formula.previusPressed === '='){
 			setFormula({previusPressed: e.target.value, formula: display + e.target.value});
-		}else{
-		setFormula(p => {return {...p, previusPressed: e.target.value, formula: p.formula + display + e.target.value}});	
+			setActiveOperators([e.target.value]);
+		}
+		else if(formula.previusPressed === e.target.value){
+			setFormula(p => ({...p, formula: p.formula.slice(0, -1) + e.target.value}));
+		}
+		else if((formula.previusPressed ==='+' || formula.previusPressed === '-' || formula.previusPressed === '*' || formula.previusPressed === '/') && e.target.value !== '-'){
+			setFormula(p => ({...p, previusPressed: e.target.value, formula: p.formula.slice(0, -activeOperators.length) + e.target.value}));
+			setActiveOperators([e.target.value]);
+		}
+		else if(display === '0' || display === '0.'){
+			setFormula(p => ({...p, previusPressed: e.target.value, formula: p.formula + e.target.value}));
+			setActiveOperators(p => ([...p, e.target.value]));	
+		}
+		else{
+			setFormula(p => ({...p, previusPressed: e.target.value, formula: p.formula + display + e.target.value}));
+			setActiveOperators([e.target.value]);	
 		}
 		setDisplay("0");
 	}
-
+	
 		const clear = () => {
 			setDisplay("0");
 			setFormula({previusPressed: "0", formula: ''})
+			setActiveOperators([]);
 		}
 
 		const calcResult = (e) => {
@@ -54,17 +77,30 @@ function App() {
 			}else{
 				let result = math.evaluate(formula.formula + display);
 				setDisplay(result);
-				setFormula({...formula, formula: formula.formula + display + e.target.value, previusPressed: e.target.value});
+				setFormula(p => ({...p, formula: p.formula + display + e.target.value, previusPressed: e.target.value}));
+				setActiveOperators([]);
 			}
+		}
+
+		const toDecimal = () => {
+			if(formula.previusPressed === '='){
+				setDisplay('0.');
+				setFormula({previusPressed: '.', formula: ''});
+			}else
+				if(display.includes('.')){
+					return;
+				}else{
+					setDisplay(p => p + '.');
+				}
 		}
 
 	return (
 		<div className='Calc'>
 			<div className='Visor'>
 				<div className='Calc-Formula'>
-						{formula.formula}
-					</div>
-				<div className='Calc-Display'>
+					{formula.formula}
+				</div>
+				<div id='display' className='Calc-Display'>
 					{display}
 				</div>
 			</div>
@@ -73,7 +109,7 @@ function App() {
 
 				<div className='Calc-Keypad-Numbers'>
 					{data.map((item) => { return <button key={item.id} id={item.name} value={item.value} onClick={numberPressed}>{item.value}</button> })}
-					<button id='decimal'>.</button>
+					<button id='decimal' onClick={toDecimal} value='.'>.</button>
 					<button id='equals' onClick={calcResult} value='='>=</button>
 				</div>
 				
